b293f8dae2d5d4ff0c91d1d4e6393211dabdd99a
Diego <dreynoso.adm@gmail.com>
2022-05-18 23:29:46 -0300

    se edita selector de entrada y se crea appnew.js

---
+++ public/js/appnew.js
@@ -0,0 +1,21 @@
+const eventos = [
+    {
+        "id": 1,
+        "evento": "Wisin y Yandel",
+        "fecha": ["12/06/2022"],
+        "entrada":[
+            { "sector": "campo", "precio": 14000, "stock": 10 },
+            { "sector": "campo-vr", "precio": 11000, "stock": 10 },
+            { "sector": "ppref", "precio": 9000, "stock": 10 },
+            { "sector": "pbaj", "precio": 7500, "stock": 10 },
+            { "sector": "palt-c", "precio": 6000, "stock": 10 },
+            { "sector": "palt-l", "precio": 5500, "stock": 10 }
+        ]
+    },
+    {
+        "id": 2, "evento": "Babasonicos", "fecha": ["15/05/2022", "16/05/2022"], "precio": 7800, "stock": 7
+    },
+    {
+        "id": 3, "evento": "Dua Lipa", "fecha": ["07/08/2022"], "precio": 14300, "stock": 3
+    }
+]
